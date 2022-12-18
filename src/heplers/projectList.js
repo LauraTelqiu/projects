11106aacf59e88fd8a197a4dfdfd5bc44fa5da53
Laura <lauratelqiu@gmail.com>
2022-12-17 23:02:18 -0500

    project list

---
+++ src/heplers/projectList.js
@@ -0,0 +1,106 @@
+import Expense from '../images/expense.png'
+import PigGame from '../images/pig-game.png'
+import GuessNum from '../images/guessnum.png'
+import FlashCards from '../images/flashCards.jpg'
+import CatFacts from '../images/catFacts.jpg'
+import PaintStore from '../images/paintStore.jpg'
+import Hater from '../images/hater.jpg'
+import TicTacToe from '../images/tic-tac-toe.jpg'
+import WhereTo from '../images/whereTo.jpg'
+import WhereToAPI from '../images/whereToApi.jpg'
+import FriendsAPI from '../images/friendsApi.jpg'
+import RestaurantAPI from '../images/restaurant.jpg'
+
+
+
+
+export const ProjectList = [
+  {
+    name: "Expense Tracker",
+    image: Expense,
+    skills: "JavaScript, React, CSS",
+    gitUrl: "https://github.com/LauraTelqiu/Expense-Tracker",
+    liveUrl: "https://expense-tracker-5b555b.netlify.app/",
+  },
+  {
+    name: "Pig Game",
+    image: PigGame,
+    skills: "HTML, CSS, JavaScript",
+    gitUrl: "https://github.com/LauraTelqiu/pig-game",
+    liveUrl: "https://piggameplaynow.netlify.app/",
+  },
+  {
+    name: "Guess My Number",
+    image: GuessNum,
+    skills: "JavaScript, CSS, HTML",
+    gitUrl: "https://github.com/LauraTelqiu/Guess-my-number",
+    liveUrl: "https://guessnumbernow.netlify.app/",
+  },
+
+  {
+    name: "Flash Cards Game",
+    image: FlashCards,
+    skills: "React, Node.js, MongoDB",
+    gitUrl: "https://github.com/LauraTelqiu/Flash-Cards-Game.git",
+    liveUrl: "https://laee.netlify.app",
+  },
+  {
+    name: "React UI Pattern",
+    image: CatFacts,
+    skills: "JavaScript, React, REST API, HTML, CSS",
+    gitUrl: "https://github.com/LauraTelqiu/React-API-UI-Pattern.git",
+    liveUrl: "https://catland-react.netlify.app/",
+  },
+
+  {
+    name: "Paint Store",
+    image: PaintStore,
+    skills: "HTMl, CSS",
+    gitUrl: "https://github.com/LauraTelqiu/Paint-Store.git",
+    liveUrl: "https://lauratelqiu.github.io/Paint-Store/",
+  },
+
+  {
+    name: "Hater - Group Project",
+    image: Hater,
+    skills: "JavaScript, SQL, Django, REST API, CSS",
+    gitUrl: "https://github.com/LauraTelqiu/hater.git",
+    liveUrl: "https://haterip.netlify.app/",
+  },
+  {
+    name: "Tic-Tac-Toe Game",
+    image: TicTacToe,
+    skills: "JavaScript, HTML, CSS",
+    gitUrl: "https://github.com/LauraTelqiu/tic-tac-toe.git",
+    liveUrl: "https://ticttactoe.netlify.app",
+  },
+  {
+    name: "CRUD WebApp",
+    image: WhereTo,
+    skills: "JavaScript, React, HTML, CSS",
+    gitUrl: "https://github.com/LauraTelqiu/Restaurant-CRUD-WebApp",
+    liveUrl: "https://whereny.netlify.app/",
+  },
+
+  {
+    name: "REST API",
+    image: WhereToAPI,
+    skills: "JavaScript, ReactJs, HTML, CSS",
+    gitUrl: "https://github.com/LauraTelqiu/friends-tv-show-api.git",
+    liveUrl: "https://friends-show-api.herokuapp.com/api/",
+  },
+  {
+    name: "JSON API",
+    image: FriendsAPI,
+    skills: "ExpressJS, Javascript, NodeJD, REST API ",
+    gitUrl: "https://github.com/LauraTelqiu/backend_api_restaurant",
+    liveUrl: "https://restaurant-backend-appication.herokuapp.com/api/",
+  },
+  {
+    name: "Restaurant Categories",
+    image: RestaurantAPI,
+    skills: "Python, REST API, Flask, SQL, PeeWee",
+    gitUrl: "https://github.com/LauraTelqiu/API-using-Python-Flask-and-SQL.git",
+    liveUrl: "",
+  },
+];
--- src/screens/about/About.js
+++ src/screens/about/About.js
@@ -1,10 +1,29 @@
 import React from 'react'
+import './about.css'
 
-function about() {
+function About() {
   return (
-    <div>
+    <div className="aboutpage" id='about'>
+      <div>
+
+        <p>
+          <br></br>
+          Software engineer with hands-on experience inspired by solving complex
+          challenges and a drive to always learn more. An organized and
+          determined team member that doesn’t fight to outshine everyone, by
+          bringing energy into collaboration and working jointly to bring the
+          project seamlessly to fruition. A former accounting clerk who brings
+          forward administrative skills that result in a more compact outcome
+          for software engineering projects. Commitment to collaboration and
+          thoroughness in accomplishing tasks, ensure that internal code and
+          user-facing products are equally enjoyable to interact with!
+        </p>
+
+        {/* <Skills /> */}
+      </div>
     </div>
-  )
+  );
 }
 
-export default about
+export default About;
+
